d85beb4b8698ff480c360745cc39fce6b1406b4d
Inon-Sinn <inon.sinn@gmail.com>
2024-05-08 20:16:55 +0300

    Class LiquidPuzzle: Finished constructPuzzle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 class liquidPuzzle:
-
     correctInput = False
 
     def __init__(self, str):
@@ -11,8 +10,45 @@ class liquidPuzzle:
         self.correctInput = True
 
     # Constructs the puzzle given be the user and returns it
+    # Currently only works on correct input
     def constructPuzzle(self, str):
-        return []
+        # Remove all whitespaces in the string for easier processing
+        str = str.replace(" ", "")
+        # just some auxiliary variables
+        puzzle = []
+        Skip, tempFirst, AddToTemp = False, 0, True
+        # We shall now go over all internal lists
+        for i in range(1, len(str) - 1):
+            # This skip a turn if found an empty internal array
+            if Skip:
+                Skip = False
+                continue
+            # check if it is an empty array
+            if str[i] == '[' and str[i + 1] == ']':
+                puzzle.append([])
+                Skip = True
+            # Check if it is the beginning of full array
+            elif str[i] == '[' and str[i + 1] != ']':
+                AddToTemp = True
+                tempFirst = i+1
+                continue
+            # Check if we are in between two internal arrays
+            elif str[i] == ',' and not AddToTemp:
+                continue
+            # Check if we are in the middle of an internal array
+            elif str[i] != ']' and AddToTemp:
+                continue
+            # if we finished an internal array we add it to the puzzle as new list
+            else:
+                AddToTemp = False
+                # cut out the internal array and turn it into an a list of integers
+                cut_str = str[tempFirst:i]
+                cut_str = cut_str.split(",")
+                for j in range(len(cut_str)):
+                    cut_str[j] = int(cut_str[j])
+                # Add it to the final product
+                puzzle.append(cut_str)
+        return puzzle
 
     # setting functions
     def setTubes(self, tubes):
@@ -34,6 +70,7 @@ class liquidPuzzle:
     def getCorrectness(self):
         return self.correctInput
 
+
 # A function that just is the interface the user will interact with
 def userInterface():
     correctInput = False
@@ -44,8 +81,10 @@ def userInterface():
             print("The Input does not stand by the rules of the Game")
         else:
             correctInput = True
+    print(puzzle.getPuzzle())
 
 
-# Just the main function, if you dont know this we have other problems
+# Just the main function, if you don't know this we have other problems
 if __name__ == '__main__':
     userInterface()
+    example = [[], [1, 4, 3, 1], [1, 4, 3, 4], [2, 2, 4, 3], [1, 2, 3, 2]]
